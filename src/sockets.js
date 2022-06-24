931730076374b9d404c21e9a61aa0df26d0ddd3b
okumuramura <bugagaga88@gmail.com>
2022-06-24 11:14:34 +0400

    bool, string and object sockets

--- src/sockets.js
+++ src/sockets.js
@@ -2,5 +2,13 @@ import Rete from "rete";
 
 export const numSocket = new Rete.Socket('Number');
 export const anySocket = new Rete.Socket('Any');
+export const objectSocket = new Rete.Socket('Object');
+export const stringSocket = new Rete.Socket('String');
+export const boolSocket = new Rete.Socket('Bool');
 
 numSocket.combineWith(anySocket);
+objectSocket.combineWith(anySocket);
+stringSocket.combineWith(anySocket);
+boolSocket.combineWith(anySocket);
+
+numSocket.combineWith(stringSocket);
